96cd1d13ba72d89b9f3a334e02d60b306d160aed
Peter Matseykanets <pmatseykanets@gmail.com>
2016-08-15 13:42:35 -0400

    Add docblocks.

--- src/Exceptions/CouldNotSendNotification.php
+++ src/Exceptions/CouldNotSendNotification.php
@@ -7,6 +7,12 @@ use GuzzleHttp\Exception\ClientException;
 
 class CouldNotSendNotification extends Exception
 {
+    /**
+     * Thrown when there's a bad request from the HttpClient.
+     *
+     * @param \GuzzleHttp\Exception\ClientException $exception
+     * @return static
+     */
     public static function hipChatRespondedWithAnError(ClientException $exception)
     {
         $code = $exception->getResponse()->getStatusCode();
@@ -15,11 +21,22 @@ class CouldNotSendNotification extends Exception
         return new static("HipChat responded with an error `{$code} - {$message}`");
     }
 
+    /**
+     * Thrown when to (room identifier) is missing.
+     *
+     * @return static
+     */
     public static function missingTo()
     {
         return new static('Notification was not sent. Room identifier is missing.');
     }
 
+    /**
+     * Thrown when an invalid message object was passed.
+     *
+     * @param mixed $message
+     * @return static
+     */
     public static function invalidMessageObject($message)
     {
         $class = get_class($message) ?: 'Unknown';
@@ -27,6 +44,11 @@ class CouldNotSendNotification extends Exception
         return new static('Notification was not sent. The message should be an instance of or extend '.HipChatMessage::class.". `Given {$class}` is invalid.");
     }
 
+    /**
+     * Thrown when any other exception is caught while sending a notification message.
+     *
+     * @return static
+     */
     public static function internalError()
     {
         return new static("Couldn't connect to HipChat API.");
